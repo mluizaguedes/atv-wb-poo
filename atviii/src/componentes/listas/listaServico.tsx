b6900b694ee37f28cb8a6377b2da75a47b181370
Maria Luiza <mluizaguedessilva@gmail.com>
2024-06-10 17:56:04 -0300

    atviii

---
+++ atviii/src/componentes/listas/listaServico.tsx
@@ -0,0 +1,133 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
+import React, { useState } from "react";
+import 'materialize-css/dist/css/materialize.min.css'
+
+type Props = {
+    tema: string
+}
+
+type Servico = {
+    id: number;
+    nome: string;
+    valor: string;
+};
+
+const ListaServico: React.FC<Props> = ({ tema }) => {
+    const [servicos, setServicos ] = useState<Servico[]>
+    ([
+        { id: 1, nome: "Manicure", valor: "R$ 30,00" },
+        { id: 2, nome: "Pedicure", valor: "R$ 35,00" },
+        { id: 3, nome: "Design de sobrancelhas", valor: "R$ 25,00" },
+        { id: 4, nome: "Corte de cabelo feminino", valor: "R$ 120,00" },
+        { id: 5, nome: "Pintura de cabelo", valor: "R$ 200,00" },
+        { id: 6, nome: "Remoção de rugas", valor: "R$ 150,00" },
+        { id: 7, nome: "Remoção de manchas na pele", valor: "R$ 200,00" },
+        { id: 8, nome: "Aplicação de Botox", valor: "R$ 500,00" },
+        { id: 9, nome: "Tratamento para emagrecimento", valor: "R$ 300,00" },
+        { id: 10, nome: "Redução de medidas", valor: "R$ 350,00" },
+        { id: 11, nome: "Corte de cabelo masculino", valor: "R$ 80,00" },
+        { id: 12, nome: "Modelagem e corte de barba", valor: "R$ 50,00" },
+        { id: 13, nome: "Tratamento para quedas de cabelo", valor: "R$ 250,00" },
+    ]);
+
+    const [servicoSelecionado, setServicoSelecionado] = useState<Servico | null>(null);
+    const [editando, setEditando] = useState(false);
+    const [servicoEditado, setServicoEditado] = useState<Servico | null>(null);
+
+
+    const servicoSelecionadoHandler = (servico: Servico) => {
+        setServicoSelecionado(servico);
+        setEditando(false);
+        setServicoEditado(servico);
+    };
+
+    const fechar = () => {
+        setServicoSelecionado(null);
+    };
+
+    const editar = () => {
+        setEditando(true);
+    };
+
+    const excluir = () => {
+        if (servicoSelecionado) {
+            const novoArrayServico = servicos.filter(servico => servico.id !== servicoSelecionado.id);
+            setServicos(novoArrayServico);
+            setServicoSelecionado(null);
+        }
+    };
+
+    const salvar = () => {
+        if (servicoSelecionado && servicoEditado) {
+            const index = servicos.findIndex(servico => servico.id === servicoSelecionado.id);
+            if (index !== -1) {
+                const novosServicos = [...servicos];
+                novosServicos[index] = servicoEditado;
+                setServicos(novosServicos);
+                setServicoSelecionado(servicoEditado);
+                setEditando(false);
+            }
+        }
+    };
+
+    const handleChange = (event: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
+        const { name, value } = event.target;
+        setServicoEditado(prevServicoEditado => ({
+            ...prevServicoEditado!,
+            [name]: value
+        }));
+    };
+
+    let estilo = `collection-item active ${tema}`;
+
+    return (
+            <div>
+                <h3 className="page-title"> World Beauty </h3>
+                <div>
+                    <h4>Serviços</h4>
+                    <div className="left">
+                        <div className="collection">
+                            {servicos.map((servico) => (
+                                <a
+                                    key={servico.nome}
+                                    className="collection-item"
+                                    onClick={() => servicoSelecionadoHandler(servico)}
+                                >
+                                    {servico.nome}
+                                </a>
+                            ))}
+                        </div>
+                    </div>
+                    <div className="table.responsive-table">
+                        {servicoSelecionado && !editando && (
+                            <div className={estilo}>
+                                <h3>{servicoSelecionado.nome}</h3>
+                                <p><strong>ID:</strong> {servicoSelecionado.id}</p>
+                                <p><strong>Preço:</strong> {servicoSelecionado.valor}</p>
+                                <button onClick={fechar} className="btn">Fechar</button>
+                                <button onClick={editar} className="btn">Editar</button>
+                                <button onClick={excluir} className="btn red">Excluir Serviço</button>
+                            </div>
+                        )}
+
+                        {servicoSelecionado && editando && (
+                            <div className={estilo}>
+                                <h3>Editar Cliente</h3>
+                                <div className="input-field">
+                                    <input type="text" name="nome" value={servicoEditado!.nome} onChange={handleChange} />
+                                    <label className="active">Nome</label>
+                                </div>
+                                <div className="input-field">
+                                    <input type="text" name="valor" value={servicoEditado!.valor} onChange={handleChange} />
+                                    <label className="active">Preço</label>
+                                </div>
+                                <button onClick={salvar} className="btn">Salvar</button>
+                                <button onClick={fechar} className="btn">Fechar</button>
+                            </div>
+                        )}
+                    </div>
+                </div>
+            </div>
+        );
+    }
+export default ListaServico;
---
+++ atviii/src/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Roteador from './componentes/roteador';
+import reportWebVitals from './reportWebVitals';
+
+ReactDOM.render(
+  <React.StrictMode> {/* modo estrito do react, que ajuda a identificar problemas potenciais na aplicação */}
+    <Roteador />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+reportWebVitals(); // chama a função reportWebVitals para medir a performance da aplicação
